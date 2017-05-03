29f2418f2fd0ae19a2e610db5a1963fd96bb1d0d
Ankit Sharma <ankit.998877@gmail.com>
2017-05-04 00:21:44 +0530

    Added

--- AlmofireinUse/ViewController.swift
+++ AlmofireinUse/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=08e64df2d3f3bc0822de1f0fc22fcb2d").responseJSON(completionHandler: { response in
+            print(response.request!)   // URL request
+            print(response.response!)  // HTTP URL response
+            print(response.data!)      // server data
+            print(response.result)     // Result of response serialization
+            
+            if let jsonResult = response.result.value
+            {
+                print("\(jsonResult)")
+            }
+            
+           })
+ 
+        
+
+        
+        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=08e64df2d3f3bc0822de1f0fc22fcb2d")
+            .validate(statusCode: 200..<300)
+            .validate(contentType: ["application/json"])
+            .responseData { response in
+                switch response.result {
+                case .success:
+                    print("Validation Successful")
+                case .failure(let error):
+                    print(error)
+                }
+        }
+        
+
+        
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
